a0587990286ef756b57ae4763774410361164cf8
rabbit6780 <rabbit6780@naver.com>
2022-08-08 15:45:46 +0900

    UniRun
    220808

--- UniRunCopy/Assets/Scripts/BackgroundLoop.cs
+++ UniRunCopy/Assets/Scripts/BackgroundLoop.cs
@@ -10,17 +10,27 @@ public class BackgroundLoop : MonoBehaviour
     private void Awake()
     {
         // 가로 길이를 측정하는 처리
+        //BoxCollider2D 컴포넌트의 Size 필드의 x값을 가로 길이로 사용
+        BoxCollider2D backgroundColider = GetComponent<BoxCollider2D>();
+
+        width = backgroundColider.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         //현재 위치가 원점에서 왼쪽으로 width 이상 이동했을 때 위치를 재배치
+        if (transform.position.x <= -width)
+        {
+            Reposition();
+        }
     }
 
     //위치를 재배치하는 메서드
     private void Reposition()
     {
-            
+        //현재 위치에서 오늘쪽으로 가로 길이 *2만큼 이동
+        Vector2 offset = new Vector2(width * 2, 0);
+        transform.position = (Vector2)transform.position + offset;
     }
 }
